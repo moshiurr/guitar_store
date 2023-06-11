fbb8abb6e9d251a9ed0896b689cea887d41381a7
Moshiur Rahman <mrahman9@upei.ca>
2023-06-11 17:00:37 -0300

    add Transaction model, migration, factory, controller and request

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Transaction;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class TransactionFactory extends Factory
+{
+    protected $model = Transaction::class;
+
+    public function definition(): array
+    {
+        return [
+            'description' => $this->faker->text(),
+            'price' => $this->faker->randomFloat(),
+            'type' => rand(0,1) ? 'income' : 'expense',
+            'user_id' => \App\Models\User::all()->random()->id,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ];
+    }
+}
