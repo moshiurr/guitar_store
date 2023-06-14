3f01d650ba3bb3bf9f998db7cfe8cdd2a323683b
Moshiur Rahman <mrahman9@upei.ca>
2023-06-14 16:55:47 -0300

    add expense component

---
+++ resources/js/Pages/Expense/Index.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import {Head} from "@inertiajs/vue3";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+</script>
+
+<template>
+
+    <Head title="Add Expense" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Add Expense</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form class="space-y-6" action="" method="POST">
+                        <div>
+                            <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Expense Name</label>
+                            <div class="mt-2">
+                                <input id="name" name="name" type="text" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                            </div>
+                        </div>
+
+                        <div>
+                            <label for="price" class="block text-sm font-medium leading-6 text-gray-900">Price</label>
+                            <div class="relative mt-2 rounded-md shadow-sm">
+                                <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                                    <span class="text-gray-500 sm:text-sm">$</span>
+                                </div>
+                                <input type="text" name="price" id="price" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="0.00" />
+                            </div>
+                        </div>
+
+                        <div>
+                            <PrimaryButton type="submit">Add</PrimaryButton>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </AuthenticatedLayout>
+
+</template>
--- routes/web.php
+++ routes/web.php
@@ -25,37 +25,55 @@ Route::get('/login', function () {
     return Inertia::render('Auth/Register');
 })->name('login');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard',[
-        'name' => 'John Doe',
-        'date' => 'June 8, 2023',
-        'budget' => '5000',
-        'total_expenses' => '2000',
-        'recent_transactions' => [
-            [
-                "id" => 2,
-                "price" => '49.47',
-                "name" => 'Atlantic Superstore',
-                "time" => '3h ago',
-                "date" => '2023-01-23T13:23Z',
-                "isDebit" => true,
-            ],
-            [
-                "id" => 1,
-                "price" => '402.67',
-                "name" => 'Walmart',
-                "time" => '3h ago',
-                "date" => '2023-01-23T13:23Z',
-                "isDebit" => false,
-            ],
-        ],
-    ]);
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard',[
+            'name' => 'John Doe',
+            'date' => 'June 8, 2023',
+            'budget' => '5000',
+            'total_expenses' => '2000',
+            'recent_transactions' => [
+                [
+                    "id" => 2,
+                    "price" => '49.47',
+                    "name" => 'Atlantic Superstore',
+                    "time" => '3h ago',
+                    "date" => '2023-01-23T13:23Z',
+                    "isDebit" => true,
+                ],
+                [
+                    "id" => 1,
+                    "price" => '402.67',
+                    "name" => 'Walmart',
+                    "time" => '3h ago',
+                    "date" => '2023-01-23T13:23Z',
+                    "isDebit" => false,
+                ],
+            ],
+        ]);
+    })->middleware(['verified'])->name('dashboard');
+
+    Route::group(['prefix' => 'expense'], function (){
+        Route::get('/', function () {
+            return Inertia::render('Expense/Index');
+        })->name('expense.index');
+
+        Route::post('/', function () {
+            return Inertia::render('Expense/Index');
+        })->name('expense.store');
+
+        Route::delete('/', function () {
+            return Inertia::render('Expense/Index');
+        })->name('expense.destroy');
+    });
+
+    Route::group(['prefix' => 'profile'], function () {
+        Route::get('/', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    });
 });
 
 require __DIR__.'/auth.php';
